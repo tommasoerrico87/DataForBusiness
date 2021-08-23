ca40173c7307cb21754be7871da672baa6518c57
tommasoerrico87 <89352565+tommasoerrico87@users.noreply.github.com>
2021-08-23 21:05:49 +0200

    Add files via upload

--- input.php
+++ input.php
@@ -1,5 +1,10 @@
 <?php
 
+$server = "sql100.epizy.com";
+$username ="epiz_29514514";
+$password ="Swye7a91lIHZ";
+$dbname ="epiz_29514514_DataForBusiness";
+
 $mysqli = new mysqli('database-dfb-mysql.cysbopuofpuh.eu-west-2.rds.amazonaws.com:3306', 'admin', 'Shido1710', 'DataForBusiness');
 
 if ($mysqli->connect_error) {
@@ -11,9 +16,8 @@ $UK = $_POST['UK'];
 $ITA = $_POST['ITA'];
 
 $toinsert = "INSERT INTO Balance
-			(Balance_UK, Balance_ITA)
 			VALUES
-			('$UK',
+			(sysdate(),'$UK',
 			'$ITA')";
 
 $result = mysqli_query($mysqli,$toinsert);	//order executes
